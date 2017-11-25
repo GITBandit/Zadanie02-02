afcc6e2a7e3a4a59b678d21b67b01ca14f007476
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-11-25 23:10:51 +0100

    Car class added. New object for car2 created.

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+class Car {
+
+    String brand, model;
+    int year;
+    double capacity;
+
+    Car(String mar, String mod, int roc, double poj) {
+        brand = mar;
+        model = mod;
+        year = roc;
+        capacity = poj;
+
+    }
+
+
+}
